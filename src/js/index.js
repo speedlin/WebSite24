83bb976744a5ecd0cd234d8fcdf9ceb16f4dfc94
speedlin <935038551@qq.com>
2020-03-06 10:23:53 +0800

    列表页完成

--- src/js/index.js
+++ src/js/index.js
@@ -19,18 +19,6 @@ var mySwiper = new Swiper('.swiper-container', {
 })  
 
 //数据渲染
-/* 
-https://o2api.jd.com/data?
-body=%7B%22query%22%3A%22query%20getCommodities(%24ids%3A%20String)%7Bcommodities(ids%3A%20%24ids)%7BgroupId%2C%20groupName%2C%20productList%7BcanSell%20skuId%20name%20image%20commentCount%20goodRate%20jdPrice%20pcpPrice%20plusPrice%20tag%20copyWriting%20copyWritingDown%20backUpWords%7D%7D%7D%22%2C%22operationName%22%3A%22getCommodities%22%2C%22variables%22%3A%7B%22ids%22%3A%22%5B09919738%2C09920106%2C09920379%2C09920437%2C09921505%2C09921604%2C09921870%2C09921901%2C09921950%2C09922019%2C09922170%5D%22%7D%2C%22config%22%3A%7B%22cache%22%3Afalse%2C%22trim%22%3Atrue%2C%22map%22%3A%7B%22keyBy%22%3A%22groupId%22%2C%22valueField%22%3A%22productList%22%7D%7D%7D
-&callback=o2237712c1f4f728d5494f9a78e4cc8454
-&_=1583329390597
-
-https://o2api.jd.com/data?
-body=%7B%22query%22%3A%22query%20getAds(%24ids%3A%20String)%7BadvertInfo(ids%3A%20%24ids)%7BgroupId%20groupName%20adList%20%7BgroupId%20pictureUrl%20link%20linkType%20advertId%7D%7D%7D%22%2C%22operationName%22%3A%22getAds%22%2C%22variables%22%3A%7B%22ids%22%3A%22%5B03569460%2C03569483%2C03569500%2C03569815%2C03569851%2C03569933%2C03569985%2C03570005%2C03570020%2C03570037%5D%22%7D%2C%22config%22%3A%7B%22cache%22%3Afalse%2C%22trim%22%3Atrue%2C%22map%22%3A%7B%22keyBy%22%3A%22groupId%22%2C%22valueField%22%3A%22adList%22%7D%7D%7D
-&callback=o2dac59b87596193cf2b96c46ff83cc8d5
-&_=1583306956052
-
-*/
 $.ajax({
     url: 'https://o2api.jd.com/data',
     data: {
@@ -54,7 +42,7 @@ $.ajax({
     success: function (res){
         // 09920379 饼干蛋糕
         // console.log(res.data['09920379']);
-        console.log(res);
+        // console.log(res);
         var str;
         for(const {image,name,pcpPrice} of res.data['09920379']){
             str = `
---
+++ src/js/list.js
@@ -0,0 +1,108 @@
+getlist();
+var flag = 0;
+var list = [];
+var oldlist = [];
+function getlist() {
+    $.ajax({
+        url: '../lib/data.json',
+        dataType: 'json',
+        success: function (res) {
+            // console.log(res);
+            list = res;
+            res.forEach(item => {
+                oldlist.push(item)
+            })
+            pagi(res);
+        }
+    })
+}
+function pagi(res){
+    $('.pagi').pagination({
+        pageCount: Math.ceil(res.length / 16),
+        current: 1,
+        coping: true,
+        homePage: '首页',
+        endPage: '末页',
+        prevContent: '上页',
+        nextContent: '下页',
+        callback: function (api) {
+            let curr = api.getCurrent()
+            console.log(curr);
+            //slice 包前不包后
+            var list = res.slice((curr - 1) * 16, curr * 16)
+            // console.log(list);
+            bindHtml(list)//每次点击
+        }
+    })
+    bindHtml(res.slice(0, 16))//第一次
+}
+function bindHtml(list) {
+    let str = '';
+
+    list.forEach(item => {
+        str += `
+            <li>
+                <div class="Goods-one"> 
+                    <img src="${item.scr}" />
+                    <p class="price">￥${item.price}</p>
+                    <p class="title">${item.title}</p>
+         	        <p class="shop"><span class="shop_name">${item.shop}</span><span class="shop_sell">已售${item.sell}</span></p>
+                    <p class="check"><a href="">查看详情</a></p>
+                </div>
+            </li>
+            `
+    })
+    $('.Goods-items').html(str)
+    // for (const { scr, price, title, shop, sell } of lists) {
+    //     str = `
+    //     <li>
+    //         <div class="Goods-one"> 
+    //           <img src="${scr}" />
+    //           <p class="price">${price}</p>
+    //           <p class="title">${title}</p>
+    // 		  <p class="shop"><span class="shop_name">${shop}</span><span class="shop_sell">已售${sell}</span></p>
+    //           <p class="check"><a href="">查看详情</a></p>
+    //         </div>
+    //       </li>
+    //     `
+    //     ul.innerHTML += list;
+    // }
+}
+var pbtn = document.querySelector('.pbtn');
+pbtn.onclick = function () {
+    $(this).addClass('active').
+    parent().siblings().children().removeClass('active');
+     
+    if (flag == 0) {
+        flag = 1;
+        pbtn.innerHTML = '价格 ↑'
+        list.sort(function(a,b){
+            return a.price - b.price;
+        })
+        pagi(list)
+        
+    }
+    else if (flag == 1) {
+        flag = 2; 
+        pbtn.innerHTML = '价格 ↓'
+        list.sort(function(a,b){
+            return b.price - a.price;
+        })
+        pagi(list)
+    }
+    else if (flag == 2) { 
+        flag = 0; 
+        pbtn.innerHTML = '价格' 
+        pagi(oldlist);
+        $(this).removeClass('active')
+        .parent().siblings().children().addClass('active');
+    }
+    
+    
+}
+var cbtn = document.querySelector('.cbtn');
+cbtn.onclick = function(){
+    $(this).addClass('active').
+    parent().siblings().children().removeClass('active');
+    $(this).parent().siblings().children().html('价格');
+}
